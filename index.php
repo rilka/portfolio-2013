277d83545b5a27ba55c4832dda5311419dc44349
Angela Li <angelaconstanceli@gmail.com>
2013-07-28 22:31:57 -0700

    added some projects

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         $b = hexdec(substr($hex, 4, 2));
 
         $r = max(0, min(255, $r + $steps));
-        $g = max(0, min(255, $g + $steps));  
+        $g = max(0, min(255, $g + $steps));
         $b = max(0, min(255, $b + $steps));
 
         $r_hex = str_pad(dechex($r), 2, '0', STR_PAD_LEFT);
@@ -27,7 +27,7 @@
 
 <!doctype html>
 
-<!-- 
+<!--
 
        .::.
        _::_
@@ -42,7 +42,7 @@
     (________)    (______)     /____\     /____\     /__\     /____\
     /________\   (________)   (______)   (______)   (____)   (______)
 
-    welcome to my source code! it's nice to meet you.
+    welcome to my source code! it's nice to meet you. :)
 
 -->
 
@@ -55,13 +55,13 @@
             <div id="noscript">Looks like you don't have Javascript enabled&mdash;what's up with that?</div>
             <style type="text/css">
                 * { visibility: hidden; }
-                #noscript { 
-                    visibility: visible !important; 
+                #noscript {
+                    visibility: visible !important;
                     margin: 10px;
                 }
             </style>
         </noscript>
-        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" type="text/css">        
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" type="text/css">
         <!--<link rel="stylesheet" type="text/css" href="styles/style.css" />-->
         <style type="text/css">
             <?php echo $s; ?>
@@ -76,8 +76,8 @@
                     <p><a href="mailto:hello@angela.li">hello@angela.li</a></p>
                 </div>
                 <div id="navigation">
-                    <a href="#about">about</a>                    
-                    <a href="#resume">r&eacute;sum&eacute;</a>          
+                    <a href="#about">about</a>
+                    <a href="#resume">r&eacute;sum&eacute;</a>
                     <a href="#work">work</a>
                     <a href="#contact">contact</a>
                 </div>
@@ -96,19 +96,19 @@
                         <h2>I really dig ...</h2>
                         <ul class="list">
                             <li>minimalism</li>
-                            <li>logic puzzles</li>                            
+                            <li>logic puzzles</li>
                             <li>souvenir shops in modern art museums</li>
                             <li>banana matcha smoothies</li>
                             <li>zeugmas</li>
-                            <li>sleeping</li>                            
+                            <li>sleeping</li>
                             <li>the IKEA catalog</li>
-                            <li><a href="http://ntris.mit.edu" target="_blank">ntris</a></li>                                                        
-                            <li>friendship-ruining board games</li>                            
+                            <li><a href="http://ntris.mit.edu" target="_blank">ntris</a></li>
+                            <li>friendship-ruining board games</li>
                         </ul>
                     </div>
                     <div id="about_img">
                         <img src="assets/images/me.jpg" />
-                    </div>                    
+                    </div>
                 </div>
                 <div id="resume_section">
                     <div class="download"><a href="assets/Angela Li - Resume.pdf">&#8595; Download PDF</a></div>
@@ -132,9 +132,9 @@
                                 <p>Handled information architecture, design prototyping, and front-end development for mobile sites.</p>
                             <h3>Quantitative Reasoning Mentor</h3>
                                 <h4>Crimson Summer Academy</h3>
-                                <h5>June 2011&mdash;May 2012</h5>   
+                                <h5>June 2011&mdash;May 2012</h5>
                                 <p>Taught pre-calculus, basic computer programming, and photography to financially disadvantaged high school students. Also served as term-time mentor and led weekly homework help sessions.</p>
-                    
+
                     </div>
                     <div id="resume_second">
                         <h2>Education</h2>
@@ -155,18 +155,18 @@
                                         <li>CS187: Computational Linguistics</li>
                                     </ul>
                                 </div>
-                            <h3>Alpharetta High School</h3> 
+                            <h3>Alpharetta High School</h3>
                                 <h4>Graduated May 2010</h4>
                                 <h5>National Merit Scholar + U.S. Presidential Scholar Semifinalist</h5>
                                 <p><strong>&rarr; </strong>Graduated May 2010. National Merit Scholar. President of symphony orchestra, Gay-Straight Alliance,  and English honor society. Volunteer at HIV/AIDS clinic and youth empowerment newspaper. Member of varsity math and quiz bowl teams.</p>
-                   
+
                         <h2>Organizations + Activities</h2>
                             <h3>The Harvard Crimson (2011&mdash;)</h3>
                             <h4>Online Design Executive <em>(former Magazine Design Chair)</em></h4>
                             <h3>Women in Computer Science (2012&mdash;)</h3>
                             <h4>Education Chair</h4>
                             <h3>Ballroom Dance Team (2010&mdash;2012)</h3>
-                            <h4>Webmaster / Judges' Liaison / competitive dancer</h4>                              
+                            <h4>Webmaster / Judges' Liaison / competitive dancer</h4>
                         <h2>Toolbox</h2>
                             <ul class="list">
                                 <li>C</li>
@@ -182,11 +182,11 @@
                                 <li>Wordpress</li>
                                 <li>Django</li>
                                 <li>Joomla</li>
-                                <li><span style="font-family:cmr10, LMRoman10-Regular, Times, serif;">L<span style="text-transform: uppercase; font-size: 70%; margin-left: -0.36em; vertical-align: 0.3em; line-height: 0; margin-right: -0.15em;">a</span>T<span style="text-transform: uppercase; margin-left: -0.1667em; vertical-align: -0.5ex; line-height: 0; margin-right: -0.125em;">e</span>X</span></li>                                
+                                <li><span style="font-family:cmr10, LMRoman10-Regular, Times, serif;">L<span style="text-transform: uppercase; font-size: 70%; margin-left: -0.36em; vertical-align: 0.3em; line-height: 0; margin-right: -0.15em;">a</span>T<span style="text-transform: uppercase; margin-left: -0.1667em; vertical-align: -0.5ex; line-height: 0; margin-right: -0.125em;">e</span>X</span></li>
                                 <li>HTML / CSS</li>
                                 <li>InDesign</li>
                                 <li>Photoshop</li>
-                                <li>Illustrator</li>                                
+                                <li>Illustrator</li>
                                 <li>Mandarin Chinese</li>
                                 <li>Spanish</li>
                                 <li>French</li>
@@ -204,16 +204,28 @@
                                 <h3>Redesign of the Harvard Investment Association website.</h3>
                             </div>
                         </a>
-                        <a href="work/onlinedesign/" target="_blank">
-                            <img src="assets/images/screenshots/odcomp.png" />
-                            <div class="caption">
-                                <h3>Landing page for the Harvard <em>Crimson</em>'s online design introduction process.</h3>
-                            </div>
-                        </a>
                         <a href="http://www.growlanka.org/" target="_blank">
                             <img src="assets/images/screenshots/growlanka.png" />
                             <div class="caption">
                                 <h3>Brochure website for nonprofit benefiting disaster victims in Sri Lanka.</h3>
+                            </div>
+                        </a>
+                        <a href="work/journalismconf/" target="_blank">
+                            <img src="assets/images/screenshots/journalismconf.png" />
+                            <div class="caption">
+                                <h3>Brochure website for The Harvard <em>Crimson</em>'s journalism conferences.</h3>
+                            </div>
+                        </a>
+                        <a href="work/cocovoice/" target="_blank">
+                            <img src="assets/images/screenshots/cocovoice.png" />
+                            <div class="caption">
+                                <h3>Mobile-friendly landing page for Coco Voice, a walkie talkie app.</h3>
+                            </div>
+                        </a>
+                        <a href="work/onlinedesign/" target="_blank">
+                            <img src="assets/images/screenshots/odcomp.png" />
+                            <div class="caption">
+                                <h3>Landing page for the Harvard <em>Crimson</em>'s online design introduction process.</h3>
                             </div>
                         </a>
                         <a href="http://www.gateswap.com/" target="_blank">
@@ -222,22 +234,28 @@
                                 <h3>Craigslist-style marketplace for college students.</h3>
                             </div>
                         </a>
+                        <a href="http://apps.facebook.com/friendship_monitor/" target="_blank">
+                            <img src="assets/images/screenshots/uff.png" />
+                            <div class="caption">
+                                <h3>Facebook defriending detection app&mdash;served nearly half a million users.</h3>
+                            </div>
+                        </a>
                         <a href="http://www.hrdctheater.com/" target="_blank">
                             <img src="assets/images/screenshots/hrdc.png" />
                             <div class="caption">
                                 <h3>A new home on the web for the Harvard-Radcliffe Dramatic Club.</h3>
                             </div>
                         </a>
-                        <a href="projects/copy_paste/" target="_blank">
+                        <a href="work/copy_paste/" target="_blank">
                             <img src="assets/images/screenshots/copypaste.png" />
                             <div class="caption">
                                 <h3>Simple text manipulation tool. (With attitude!)</h3>
                             </div>
                         </a>
-                        <a href="http://www.unfriendfinder.me/" target="_blank">
-                            <img src="assets/images/screenshots/uff.png" />
+                        <a href="work/ver1/" target="_blank">
+                            <img src="assets/images/screenshots/ver1.png" />
                             <div class="caption">
-                                <h3>Facebook defriending detection app&mdash;served nearly half a million users.</h3>
+                                <h3>First iteration of personal website&mdash;still alive for the curious!</h3>
                             </div>
                         </a>
                         <a href="http://www.cs171.org/2012/final-projects/web/10_CollegeSmash/" target="_blank">
@@ -246,50 +264,44 @@
                                 <h3>Visualization tool for filtering and comparison of colleges in the United States.</h3>
                             </div>
                         </a>
-                        <a href="projects/ver1/" target="_blank">
-                            <img src="assets/images/screenshots/ver1.png" />
-                            <div class="caption">
-                                <h3>First iteration of personal website&mdash;still alive for the curious!</h3>
-                            </div>
-                        </a>
                     </div>
                 </div>
                 <div id="contact_section">
                     <div id="contact_side">
                         <h2>Social Butterfly</h2>
-                        <a href="http://www.linkedin.com/in/angelaconstance">i</a> 
-                        <a href="http://angelaconstance.wordpress.com">w</a>  
-                        <a href="https://www.facebook.com/angelaconstanceli">f</a> 
-                        <a href="https://twitter.com/angelaconstance">t</a> 
-                        <a href="http://fauvism.tumblr.com">o</a> 
-                        <a href="http://pinterest.com/angelaconstance">&</a> 
+                        <a href="http://www.linkedin.com/in/angelaconstance">i</a>
+                        <a href="http://angelaconstance.wordpress.com">w</a>
+                        <a href="https://www.facebook.com/angelaconstanceli">f</a>
+                        <a href="https://twitter.com/angelaconstance">t</a>
+                        <a href="http://fauvism.tumblr.com">o</a>
+                        <a href="http://pinterest.com/angelaconstance">&</a>
                         <p><a href="https://github.com/angelali">Fork me on GitHub</a></p>
                     </div>
                     <div id="contact_main">
                         <h2>Owl Post</h2>
                         <p>I love getting mail, even if it isn't scratched out on parchment and sealed with wax. I read every word that passes through my inbox and promise to respond to your message within 24 hours&mdash;less if there isn't a storm brewing. So type away!</p>
                         <p>You can also email me directly at <a href="mailto:hello@angela.li">hello@angela.li</a>.</p>
-                        <div id="contact_form">  
-                            <form name="contact" action="" method="">  
-                              <fieldset>  
+                        <div id="contact_form">
+                            <form name="contact" action="" method="">
+                              <fieldset>
                                 <label class="field" for="name">Name:</label>
                                 <input type="text" name="name" id="name" value="" />
                                 <label class="error" for="name">&nbsp; *</label>
-                                
+
                                 <label class="field" for="email">Email:</label>
                                 <input type="email" name="email" id="email" value="" />
                                 <label class="error" for="email">&nbsp; *</label>
-                                
+
                                 <label class="field" for="message">Message:</label>
                                 <textarea name="message" id="message" value="" rows=7 /></textarea>
                                 <label class="error" for="message">&nbsp; *</label>
-                                
+
                                 <div class="clear"></div>
 
                                 <input type="submit" name="submit" class="button" value="Off you go, Hedwig!" /><div id="response"></div>
-                              </fieldset>  
-                            </form>  
-                        </div>                                                  
+                              </fieldset>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
